14f96bbaf0b58b3e7fe97727f6c826372fa114fb
wangheng <673602165@qq.com>
2022-11-05 22:46:45 +0800

    change linux

---
+++ simulation/experiment_util.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    return torch.device('cpu')
+
+
+def to_device(data: torch.Tensor, device):
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+def accuracy(predicted: torch.Tensor, actual: torch.Tensor):
+    max_values, max_indices = torch.max(predicted, dim=1)
+    return torch.tensor(torch.sum(max_indices == actual).item() / len(max_indices))
