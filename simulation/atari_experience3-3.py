fe6790c030a380ba75afbf7356588e196e2626af
wangheng <673602165@qq.com>
2022-12-07 19:28:37 +0800

    newest

--- simulation/atari_experience3-3.py
+++ simulation/atari_experience3-3.py
@@ -50,6 +50,32 @@ class NormalCNN3(BaseFeaturesExtractor):
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
         return self.linear(self.cnn(observations))
 
+
+class NormalCNN5(BaseFeaturesExtractor):
+
+    def __init__(self, observation_space: gym.Space, features_dim: int = 64):
+        super(NormalCNN5, self).__init__(observation_space, features_dim)
+        n_input_channel = observation_space.shape[0]
+        # print(observation_space.shape)
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channel, 64, 3, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(),
+        )
+        with torch.no_grad():
+            # print(observation_space.sample()[None].shape)
+            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
+
+
 class NormalCNN7(BaseFeaturesExtractor):
 
     def __init__(self, observation_space: gym.Space, features_dim: int = 64):
@@ -75,61 +101,34 @@ class NormalCNN7(BaseFeaturesExtractor):
         return self.linear(self.cnn(observations))
 
 
-class RestCNN3(BaseFeaturesExtractor):
-
-    def __init__(self, observation_space: gym.Space, features_dim: int = 64):
-        super(RestCNN3, self).__init__(observation_space, features_dim)
-        n_input_channel = observation_space.shape[0]
-        # print(observation_space.shape)
-        self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channel, 64, 3, 1),
-            ResNetBlock(64,64),
-            nn.MaxPool2d(2, 2),
-            ResNetBlock(64,64),
-            nn.MaxPool2d(2, 2),
-            ResNetBlock(64,64),
-            nn.Flatten(),
-        )
-        with torch.no_grad():
-            # print(observation_space.sample()[None].shape)
-            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
-        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
-
-    def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        return self.linear(self.cnn(observations))
-
-
-def train_a2c(envname: str, logdir:str, policy_kwargs:dict=None):
-    print("开始训练,"+' env='+envname +', logdir: '+logdir)
+def train_a2c(envname: str, logdir: str, policy_kwargs: dict = None):
+    print("开始训练," + ' env=' + envname + ', logdir: ' + logdir)
     env = make_atari_env(envname, n_envs=3)
     env = VecFrameStack(env, n_stack=6)
     # , policy_kwargs=policy_kwargs
-    model = A2C('CnnPolicy', env, tensorboard_log=logdir, policy_kwargs=policy_kwargs, device='cuda')
+    if policy_kwargs is None:
+        model = A2C('CnnPolicy', env, tensorboard_log=logdir, device='cuda')
+    else:
+        model = A2C('CnnPolicy', env, tensorboard_log=logdir, policy_kwargs=policy_kwargs, device='cuda')
     model.learn(800_000)
 
 
 if __name__ == '__main__':
     normal_m3_policy_kwargs = dict(features_extractor_class=NormalCNN3, features_extractor_kwargs=dict(features_dim=64))
-    normal_m3 = Thread(target=train_a2c, args=('ALE/Assault-v5','./assaultv5', normal_m3_policy_kwargs))
+    normal_m3 = Thread(target=train_a2c, args=('ALE/Assault-v5', './assaultv5', None))
     normal_m3.start()
     time.sleep(10)
 
-    normal_m5 = Thread(target=train_a2c, args=('ALE/Assault-v5','./assaultv5'))
+    normal_m5_policy_kwargs = dict(features_extractor_class=NormalCNN5, features_extractor_kwargs=dict(features_dim=64))
+    normal_m5 = Thread(target=train_a2c, args=('ALE/Assault-v5', './assaultv5'))
     normal_m5.start()
     time.sleep(10)
 
-    normal_m7_policy_kwargs = dict(features_extractor_class=NormalCNN3, features_extractor_kwargs=dict(features_dim=64))
-    normal_m7 = Thread(target=train_a2c, args=('ALE/Assault-v5','./assaultv5', normal_m7_policy_kwargs))
+    normal_m7_policy_kwargs = dict(features_extractor_class=NormalCNN7, features_extractor_kwargs=dict(features_dim=64))
+    normal_m7 = Thread(target=train_a2c, args=('ALE/Assault-v5', './assaultv5', normal_m7_policy_kwargs))
     normal_m7.start()
     time.sleep(10)
 
     normal_m3.join()
     normal_m5.join()
     normal_m7.join()
-
-    
-    
-
-
-# plot_atari_image()
-train_sac()
