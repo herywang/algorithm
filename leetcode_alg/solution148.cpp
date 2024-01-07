11f2836e6d8f74ee80f2b9bc4d358beeccd2573f
wangheng <673602165@qq.com>
2024-01-07 13:09:43 +0800

    feature: solution 148 链表排序

--- leetcode_alg/solution148.cpp
+++ leetcode_alg/solution148.cpp
@@ -9,6 +9,7 @@
  *
  */
 #include <iostream>
+#include <algorithm>
 #include "listnode.h"
 
 using namespace std;
@@ -19,28 +20,56 @@ public:
         if (head == nullptr || head->next == nullptr) {
             return head;
         }
-        ListNode *dummyHead = new ListNode(-1, head);
-        ListNode *p = head->next;
-        head->next = nullptr;
-        while (p != nullptr) {
-            ListNode *t = dummyHead;
-            while (t->next != nullptr && p->val > t->next->val) {
-                t = t->next;
-            }
-            ListNode *tmp = p;
+        vector<int> array;
+        ListNode* p = head;
+        while(p != nullptr){
+            array.push_back(p->val);
             p = p->next;
-            tmp->next = t->next;
-            t->next = tmp;
         }
-        return dummyHead->next;
+        std::sort(array.begin(), array.end());
+//        sort(array, 0, array.size()-1);
+        p = head;
+        int i = 0;
+        while (p != nullptr){
+            p->val = array[i++];
+            p = p->next;
+        }
+        return head;
+    }
+
+private:
+    void sort(vector<int> &array, int left, int right){
+        if(left >= right){
+            return;
+        }
+        int l = left;
+        int r = right;
+        int baseVal = array[left];
+        while(l < r){
+            // 从右往左找第一个比baseVal小的值
+            while(r > l && array[r] >= baseVal){
+                r--;
+            }
+            array[l] = array[r];
+            // 从左往右找第一个比baseVal大的值
+            while(l < r && array[l] <= baseVal){
+                l++;
+            }
+            array[r] = array[l];
+        }
+        array[l] = baseVal;
+        sort(array, left, l-1);
+        sort(array, l+1, right);
     }
 };
 
 int main() {
     auto s = new Solution;
-    ListNode *head = new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
+    vector<int> array = {4,19,14,5,-3,1,8,5,11,15};
+    ListNode *head = buildList(array);
     ListNode *res = s->sortList(head);
     print_list(res);
+    free_list(head);
     delete s;
     return 0;
 }
